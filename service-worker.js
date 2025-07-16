0c5980ebad3087e99652eb67fb15f1222cdfed29
Reaper2719 <jdiazr2704@gmail.com>
2025-07-16 16:45:24 -0500

    fp 17

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,41 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("modulo1-cache").then(cache => {
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('modulo1-cache-v2').then(cache => {
       return cache.addAll([
-        "index.html",
-        "style.css",
-        "main.js",
-        "indexedDB.js",
-        "export.js",
-        "manifest.json",
-        "icon-192.png",
-        "icon-512.png"
+        '/',
+        'index.html',
+        'style.css',
+        'main.js',
+        'indexedDB.js',
+        'export.js',
+        'manifest.json',
+        'icon-192.png',
+        'icon-512.png'
       ]);
     })
   );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== 'modulo1-cache-v2').map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).catch(() => {
+        if (event.request.destination === 'document') {
+          return caches.match('index.html');
+        }
+      });
+    })
   );
 });
